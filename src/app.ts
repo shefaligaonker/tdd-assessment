1879e2c9ef0f37f8f18490641f5f61af0cdb9722
Shefali Gaonker <Shefali.Gaonker@ibm.com>
2025-01-03 17:25:14 +0530

    Add numbers, support different delimiters

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,23 @@
  * @returns sum
  */
 export function add (input: string): number {
-    const items = input.split(/,|\n/)
+    let items: string[] = [] 
+    
+    // extract delimiter
+    const regex = /\/\/(.*?)\n/ 
+    const parts: string[] = input.match(regex) || []
+
+    if(parts[1]){
+        // custom delimiter
+        input = input.replace(regex, '')
+        const dynamicReg = new RegExp(`,|\n|${parts[1]}`)  
+        items = input.split(dynamicReg)    
+    }
+    else{
+        // split based on , or \n
+        items = input.split(/,|\n/)
+    }
+
     let result = 0
     items.forEach( element => {
         result = result + parseInt(element)
--- tests/add.test.ts
+++ tests/add.test.ts
@@ -11,3 +11,11 @@ test('Should add multiple numbers', () => {
 test('Should add two numbers with \n', () => {
     expect(add('1\n2,3')).toBe(6)
 })
+
+test('Should add two numbers with custom delimiters', () => {
+    expect(add('//;\n1;2')).toBe(3)
+})
+
+test('Should add two numbers with different delimiters', () => {
+    expect(add('//;\n1;2,1\n1')).toBe(5)
+})
